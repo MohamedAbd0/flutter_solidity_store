50f22ad1c630439e8a16f0282a07ac1164ac5500
mabdo <mohamed_abdo95@outlook.com>
2025-04-27 02:16:36 +0400

    handling app config value

--- lib/core/di/app_config.dart
+++ lib/core/di/app_config.dart
@@ -1,9 +1,28 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
 class AppConfig {
-  final String baseUrl;
+  /// Contract Details (SEPOLIA)
+  final String contractAddress;
+  final String contractAbi;
+
+  /// Network Details (INFURA)
+  final int chainId;
+  final String rpcHttpsUrl;
+  final String rpcSocketUrl;
+
+  /// IPFS Deails (PINATA)
+  final String pinataApiKey;
+  final String pinataApiSecretKey;
+  final String pinataGatewayUrl;
 
   AppConfig({
-    required this.baseUrl,
+    required this.contractAddress,
+    required this.contractAbi,
+    required this.chainId,
+    required this.rpcHttpsUrl,
+    required this.rpcSocketUrl,
+    required this.pinataApiKey,
+    required this.pinataApiSecretKey,
+    required this.pinataGatewayUrl,
   });
 }
--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,22 +1,17 @@
 class Config {
   /// Contract Details (SEPOLIA)
-  static String CONTRACT_ADDRESS = "";
-  static String CONTRACT_ABI = "";
+  static String CONTRACT_ADDRESS = "YOUR_CONTRACT_ADDRESS";
+  static String CONTRACT_ABI = "assets/abis/FlutterSolidityStore.json";
 
   /// Network Details (INFURA)
-  static String CHAIN_ID = "";
-  static String RPC_HTTPS_URL = "";
-  static String PRC_SOCKET_URL = "";
+  static int CHAIN_ID = 11155111;
+  static String RPC_HTTPS_URL = "https://sepolia.infura.io/v3/YOUR_ID";
+  static String PRC_SOCKET_URL = "wss://sepolia.infura.io/ws/v3/YOUR_ID";
 
   /// IPFS Deails (PINATA)
-  static String PINATA_API_KEY = "9094e9cb0a1e83346b36";
-  static String PINATA_API_SECRET =
-      "fb9bbc161de6754a129a855220d46e63bd6e79a28be5301d02cbe61290115ac3";
-  static String PINATA_JWT =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI3NTExM2ZhOS05NzY3LTQ3NWQtYjdiYi01NjJhNjZiNGQ1Y2QiLCJlbWFpbCI6Im1vaGFtZWRhYmRvZWxuYXNoYXJAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiRlJBMSJ9LHsiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjEsImlkIjoiTllDMSJ9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6IjkwOTRlOWNiMGExZTgzMzQ2YjM2Iiwic2NvcGVkS2V5U2VjcmV0IjoiZmI5YmJjMTYxZGU2NzU0YTEyOWE4NTUyMjBkNDZlNjNiZDZlNzlhMjhiZTUzMDFkMDJjYmU2MTI5MDExNWFjMyIsImV4cCI6MTc3NjU2MDExM30.FpbSYboIOKudssNPRGVNLmhNbDCOOUJVMkNnptUCtk8";
+  static String PINATA_API_KEY = "YOUR_API_KEY";
+  static String PINATA_API_SECRET = "YOUR_API_SECRET";
+  static String PINATA_JWT = "YOUR_PINATA_JWT";
 
-  static String PINATA_GATEWAY_URL =
-      "https://beige-giant-clownfish-525.mypinata.cloud";
-
-  /// https://beige-giant-clownfish-525.mypinata.cloud/ipfs/bafybeicr2xu5vqwx3dqexmjgjx5uu6gyp64zurwnefongi2s2wwkdndd5a
+  static String PINATA_GATEWAY_URL = "https://PROJECT_ID-525.mypinata.cloud";
 }
